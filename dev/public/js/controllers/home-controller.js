f8559e54c3afdfac10388e3fea45d7a760223299
qusaisafa <qusi.bassam@gmail.com>
2018-05-26 04:34:53 +0300

    C#3 : update comment

--- dev/public/js/controllers/home-controller.js
+++ dev/public/js/controllers/home-controller.js
@@ -46,6 +46,52 @@
                 $scope.cancel();
             }
         }
+        //---------------update------------//
+        // update comment record / save changes
+        // retrieve record to fill out the form
+        $scope.showUpdateCommentForm = function(data){
+
+            // get comment to be edited
+            // commentsService.readOneProduct(data).then(function successCallback(response){
+
+                // put the values in form
+                $scope.id = data.id;
+                $scope.name = data.name;
+                $scope.body = data.body;
+                $scope.email = data.email;
+
+                $mdDialog.show({
+                    controller: DialogController,
+                    templateUrl: '../../partials/update_comment.template.html',
+                    parent: angular.element(document.body),
+                    targetEvent: event,
+                    clickOutsideToClose: true,
+                    scope: $scope,
+                    preserveScope: true,
+                    fullscreen: true
+                }).then(
+                    function(){},
+
+                    // user clicked 'Cancel'
+                    function() {
+                        // clear modal content
+                        $scope.id = "";
+                        $scope.name = "";
+                        $scope.body = "";
+                        $scope.email = "";
+                    }
+                );
+
+            // }, function errorCallback(response){
+            //     $scope.showToast("Unable to retrieve record.");
+            // });
+
+        }
+
+        $scope.updateComment = function(){
+            // TODO call service update method to send update request to server
+            $scope.cancel();
+        }
 
     }]);
 })(Comments);
